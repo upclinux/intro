5f6a80237ee17b95a7f9b1b04313020ef8cfeb56
Siyang Li <vjudge404@gmail.com>
2015-10-03 22:03:06 +0800

    修改样式

--- js/callout.js
+++ js/callout.js
@@ -1,14 +1,29 @@
-function callout(){
+(function (){
   var ele = document.querySelector('article');
   if (ele === null) return false;
 
   var text = ele.innerHTML;
-  text = text.replace(/<p>\s*?\[%\s*?callout\s*?%\]\s*?<\/p>/g, '<div class="callout callout-primary">');
-  text = text.replace(/<p>\s*?\[%\s*?endcallout\s*?%\]\s*?<\/p>/g, '</div>');
-
-  var reg = /<p>\s*?\[%\s*?callout\s+?style\s*?=\s*?["]{0,1}(.*?)["]{0,1}\s*?%\]\s*?<\/p>/;
   var style;
-  while (style=reg.exec(text)) text=text.replace(reg, '<div class="callout callout-'+style[1]+'">');
+  var reg,reg2;
+  // Callout start
+  text = text.replace(/<p>\s*?\[%\s*?callout\s*?%\]\s*?<\/p>/g, '<div class="callout callout-primary">');
+  reg = /<p>\s*?\[%\s*?callout\s*?%\]\s*?(.*?)<\/p>/;
+  while (style=reg.exec(text)) {
+    text=text.replace(reg, '<div class="callout callout-primary"><h4>'+style[1]+'</h4>');
+  }
+
+  reg = /<p>\s*?\[%\s*?callout\s+?style\s*?=\s*?["]{0,1}(.*?)["]{0,1}\s*?%\]\s*?<\/p>/;
+  var reg2 = /<p>\s*?\[%\s*?callout\s+?style\s*?=\s*?["]{0,1}(.*?)["]{0,1}\s*?%\]\s*?(.*?)<\/p>/;
+  while (style=reg.exec(text)) {
+    text=text.replace(reg, '<div class="callout callout-'+style[1]+'">');
+  }
+  while (style=reg2.exec(text)) {
+    text=text.replace(reg2, '<div class="callout callout-'+style[1]+'"><h4>'+style[2]+'</h4>');
+  }
+
+  // endcallout
+  text = text.replace(/<p>\s*?\[%\s*?endcallout\s*?%\]\s*?<\/p>/g, '</div>');
+  text = text.replace(/\s*?\[%\s*?endcallout\s*?%\]\s*?<\/p>/g, '</p></div>');
 
   ele.innerHTML = text;
-};callout();
+})();
